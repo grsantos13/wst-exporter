6299c804698f69ee338f4531a9a7ad7b8af51567
Gustavo Santos <gsantoset@gmail.com>
2021-04-12 12:29:03 -0300

    Remove os exportadores pendentes de cadastro no momento do cadastro de um.

--- src/main/kotlin/br/com/gn/exporter/ExporterService.kt
+++ src/main/kotlin/br/com/gn/exporter/ExporterService.kt
@@ -1,11 +1,13 @@
 package br.com.gn.exporter
 
+import br.com.gn.exporter.register.Register
 import br.com.gn.shared.exception.ObjectAlreadyExistsException
 import br.com.gn.shared.exception.ObjectNotFoundException
 import br.com.gn.shared.validation.ValidUUID
 import io.micronaut.validation.Validated
 import java.util.*
 import javax.inject.Singleton
+import javax.persistence.EntityManager
 import javax.transaction.Transactional
 import javax.validation.Valid
 import javax.validation.constraints.NotBlank
@@ -13,14 +15,20 @@ import javax.validation.constraints.NotBlank
 @Validated
 @Singleton
 class ExporterService(
-    private val repository: ExporterRepository
+    private val repository: ExporterRepository,
+    private val manager: EntityManager
 ) {
 
     @Transactional
     fun create(@Valid request: NewExporterRequest): Exporter {
-        val existsByCode = repository.existsByCode(request.code)
-        if (existsByCode)
+        if (repository.existsByCode(request.code))
             throw ObjectAlreadyExistsException("Exporter already exists with code ${request.code}")
+
+        // Remove dos exportadores pendentes de cadastro
+        manager.createQuery(" select r from Register r where r.code = :code ", Register::class.java)
+            .setParameter("code", request.code)
+            .resultList
+            .map(manager::remove)
 
         val exporter = request.toModel()
         repository.save(exporter)
